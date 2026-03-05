7861bb10f4a4af3d4cd3863625cecf2d6e6a6a50
jessjames2023-oss <jess.james2023@vitstudent.ac.in>
2026-03-05 17:25:55 +0530

    Update MaxNumberTest.java

--- src/test/java/MaxNumberTest.java
+++ src/test/java/MaxNumberTest.java
@@ -1,5 +1,7 @@
+package com.example
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
 public class MaxNumberTest {
     @Test
     public void testMaxWithFirstNumberGreater() {
