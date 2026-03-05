f645e78f9e0af9895816694652f89211c0846ddd
lokeshwari2209 <lokeshwari2209@gmail.com>
2026-03-05 16:28:23 +0530

    adding

---
+++ src/test/java/MaxNumberTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MaxNumberTest {
+
+    @Test
+    public void testMaxWithFirstNumberGreater() {
+        MaxNumber maxNumber = new MaxNumber();
+        assertEquals(10, maxNumber.max(10, 5));
+    
+}
